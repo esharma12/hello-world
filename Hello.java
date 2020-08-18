5f2f8d29125fcac8129f974534207168feac0cfa
esharma12 <69831724+esharma12@users.noreply.github.com>
2020-08-17 22:54:06 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/********************************************************************
+* Hello 
+********************************************************************
+* Esha Sharma 
+* 08/17/2020
+* CMSC 255 - C90
+********************************************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("<Your Name>");
+		System.out.println("<Java Project Name>");
+		System.out.println("<Project Description>"); 
+		System.out.println("<Version Date>"); 
+		System.out.println("<Course Number and Section>"); 
+		System.out.println();
+	}
+}
